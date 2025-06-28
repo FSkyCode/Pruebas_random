9396b0a2c28250c0af9c615e4f06a8358aaaab55
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-06-28 16:09:15 -0500

    Actualizar decisiones.js

--- js/decisiones.js
+++ js/decisiones.js
@@ -1,1 +1,6 @@
-hi2
+// Guardar decisión
+localStorage.setItem("decision_1", "ayudó");
+
+// Leer decisión
+let d1 = localStorage.getItem("decision_1");
+console.log("En la escena 1, el jugador: " + d1);
